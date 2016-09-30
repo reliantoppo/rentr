ad3fec743508aded8f21f0a7e3906e4e2f3eb14c
Justin Simonelli <justin.simonelli@gmail.com>
2016-09-30 08:35:13 -0700

    Making config file private

--- send.py
+++ send.py
@@ -8,6 +8,7 @@ import json
 import requests
 
 logger = logging.getLogger(__name__)
+config = None
 
 MESSAGE = """Hey {0}! It's that time again, time to pay rent. You owe ${1} this month. Pay with the link below, thanks!
 
@@ -21,34 +22,40 @@ NOT_CONFIGURED_MESSAGE = """Cannot initialize Twilio notification
 middleware. Required enviroment variables TWILIO_ACCOUNT_SID, or
 TWILIO_AUTH_TOKEN or TWILIO_NUMBER missing. Don't forget to bring the private directory over!"""
 
+def load_json_config():
+    with open('config/private/config.json') as configFile:
+        global config
+        config = json.load(configFile)
+        if not(config):
+            raise Exception('Unable to read config file!')
 
-def load_renters_file():
-    with open('config/renters.json') as rentersFile:
-        renters = json.load(rentersFile)
-        if not(renters):
-            raise Exception('Cannot read renters json file!')
-        return renters
+def get_renters():
+
+    renters = config['renters']
+    if not (renters):
+        raise Exception('Cannot read renters json file!')
+    return renters
 
 
-def load_twilio_config():
-    with open('config/private/twilio.json') as twilioFile:
-        config = json.load(twilioFile);
+def twilio_config():
+    twilio = config['twilio']
+    twilio_account_sid = twilio['TWILIO_ACCOUNT_SID']
+    twilio_auth_token = twilio['TWILIO_AUTH_TOKEN']
+    twilio_number = twilio['TWILIO_NUMBER']
 
-        twilio_account_sid = config['TWILIO_ACCOUNT_SID']
-        twilio_auth_token = config['TWILIO_AUTH_TOKEN']
-        twilio_number = config['TWILIO_NUMBER']
+    if not all([twilio_account_sid, twilio_auth_token, twilio_number]):
+        logger.error(NOT_CONFIGURED_MESSAGE)
+        raise Exception(NOT_CONFIGURED_MESSAGE)
 
-        if not all([twilio_account_sid, twilio_auth_token, twilio_number]):
-            logger.error(NOT_CONFIGURED_MESSAGE)
-            raise Exception(NOT_CONFIGURED_MESSAGE)
+    return (twilio_number, twilio_account_sid, twilio_auth_token)
 
-        return (twilio_number, twilio_account_sid, twilio_auth_token)
+
 
 
 class MessageClient(object):
     def __init__(self):
         (twilio_number, twilio_account_sid,
-         twilio_auth_token) = load_twilio_config()
+         twilio_auth_token) = twilio_config()
 
         self.twilio_number = twilio_number
         self.twilio_client = TwilioRestClient(twilio_account_sid,
@@ -59,10 +66,11 @@ class MessageClient(object):
                                            from_=self.twilio_number
                                            # media_url=['https://demo.twilio.com/owl.png'])
                                            )
-def notify_everyone():
-    print("notify_everyone(): running..")
+def notify():
+    print("notify(): running..")
+    load_json_config()
 
-    renters = load_renters_file()
+    renters = get_renters()
     client = MessageClient()
 
     for person in renters:
@@ -71,10 +79,10 @@ def notify_everyone():
         #params = json.dumps({'longUrl': url_to_shorten})
         #response = requests.post(post_url, params, headers={'Content-Type': 'application/json'})
 
-        client.send_message(message_to_send, person['phone_number'])
+        #client.send_message(message_to_send, person['phone_number'])
 
     print("notifications complete!")
 
     return None
 
-notify_everyone();
+notify();
